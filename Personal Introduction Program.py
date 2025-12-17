63208f7b91cb31a5054b127edab97b6b66191b67
Sakshinarde25 <130461559+Sakshinarde25@users.noreply.github.com>
2025-12-17 16:50:20 +0530

    Add files via upload

---
+++ Personal Introduction Program.py
@@ -0,0 +1,11 @@
+# Personal Introduction Program
+# Week 1 Python Project
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city are you from? ")
+
+print("\n Welcome", name + "! ")
+print(f"You are {age} years old, live in {city}, and love {hobby}.")
+print("Nice to meet you! ")
